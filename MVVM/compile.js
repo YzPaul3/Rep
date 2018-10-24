48739cccfd703769b3c7cdf255634f772cd8c37a
YzPaul3 <heyuzepaul@live.cn>
2018-10-24 17:38:59 +0800

    mvvm

--- MVVM/compile.js
+++ MVVM/compile.js
@@ -5,7 +5,7 @@ class Compile {
 
         // 如果元素存在，才开始编译
         if (this.el) {
-            // 1.先把真是的DOM移入到内存中
+            // 1.先把真实的DOM移入到内存中
             let fragment = this.node2fragment(this.el)
 
             // 2.提取带有指令的元素节点（如v-model）和 文本模板节点{{text}}
@@ -67,9 +67,10 @@ class Compile {
             let attrName = attr.name
             if (this.isDirective(attrName)) {
                 // 取对应的值放入节点中
-                let expr = attr.value
-                let type = attr.name.slice(2)
+                let expr = attr.value   // message.msg
+                let type = attr.name.slice(2) // model
                 // node this.vm.$data expr
+                // 执行编译
                 CompileUtil[type](node, this.vm, expr)
             }
         })
@@ -78,7 +79,6 @@ class Compile {
     // 编译文本
     compileText(node) {
         let text = node.textContent
-        console.log(text)
         let reg = /\{\{([^}]+)\}\}/g
         if (reg.test(text)) {
             // node this.vm.$data text
@@ -88,9 +88,11 @@ class Compile {
 }
 
 CompileUtil = {
+    // 获取实例上对应的数据
     getVal(vm, expr) {
-        // 获取实例上对应的数据
+        // expr: 'message.msg'
         expr = expr.split('.')
+        // array: ['message', 'msg']
         return expr.reduce((prev, next) => {
             return prev[next]
         }, vm.$data)
@@ -103,18 +105,20 @@ CompileUtil = {
     text(node, vm, expr) {
         let updateFn = this.updater['textUpdater']
         let value = this.getTextVal(vm, expr)
-
         // {{a}}{{b}}{{c}}
-        // 添加观察者
+        // 给这个节点的每一个模板添加观察者  {{message.msg}}  {{message.msg1}}
         expr.replace(/\{\{([^}]+)\}\}/g, (...args) => {
             new Watcher(vm, args[1], (newValue) => {
-                // 数据变化，文本节点需要重新获取依赖的属性更新文本内容
+                // 这里是watcher数据变化时的回调
+                // 数据变化时,文本节点需要重新获取依赖的属性,更新text文本内容
                 updateFn && updateFn(node, this.getTextVal(vm, expr))
             })
         })
-        updateFn && updateFn(node, value)
+        updateFn && updateFn(node, this.getTextVal(vm, expr))
     },
     setVal(vm, expr, value) {
+        // 将vm.$data中的expr表达式的值修改为监听input事件中获取到的输入框的值
+        // 即：vm.$data.message.msg = 正在input的值
         expr = expr.split('.')
         return expr.reduce((prev, next, currentIndex) => {
             if (currentIndex === expr.length -1) {
@@ -125,7 +129,7 @@ CompileUtil = {
     },
     model(node, vm, expr) {
         let updateFn = this.updater['modelUpdater']
-        // 添加观察者
+        // 给当前v-model节点添加观察者
         new Watcher(vm, expr, (newValue) => {
             updateFn && updateFn(node, this.getVal(vm, expr))
         })
--- MVVM/watcher.js
+++ MVVM/watcher.js
@@ -1,4 +1,4 @@
-// 给需要变化的元素添加观察者，当数据变化时
+// 给需要变化的元素v-xxx,{{}}添加观察者，当数据变化时
 class Watcher{
     constructor(vm, expr, cb) {
         this.vm = vm
@@ -9,7 +9,7 @@ class Watcher{
     }
 
     getVal(vm, expr) {
-        // 获取实例上对应的数据
+        // 获取实例上对应的数据 比如message.msg的值
         expr = expr.split('.')
         return expr.reduce((prev, next) => {
             return prev[next]
